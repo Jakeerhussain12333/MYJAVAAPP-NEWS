9b1633c4ed35ea90c7d8b42dbfb673293a21ad79
Jakeerhussain12333 <jakeerh32@gmail.com>
2024-08-04 15:05:51 +0530

    Update

--- pythonlife.java
+++ pythonlife.java
@@ -1,5 +1,7 @@
 public class pythonlife {
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
   }
 }
